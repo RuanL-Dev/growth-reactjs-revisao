010947d390ead0879c698c0a18e4a1b64f69ae7e
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-10-15 14:34:45 -0300

    Criado exemplo com Scss

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,13 @@
 import Title from '../src/components/title/Title'
+import Button from '../src/components/button/Button'
 
 export default function HomePage() {
-  return <Title>Título da Página</Title>
+  return (
+    <>
+      <Title>Título da Página</Title>
+      <Button>Botão</Button>
+      <label>Label Teste</label>
+      <button>Button</button>
+    </>
+  )
 }
