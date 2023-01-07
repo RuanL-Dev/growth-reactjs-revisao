7af3bb3ee9c0d0d913c9a49f400d94655180abe2
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2023-01-07 16:39:29 -0300

    Criado exemplo com useRef

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,9 +1,13 @@
+import { forwardRef } from 'react'
 import styled from 'styled-components'
 
 const StyledInput = styled.input`
   padding: 10px 20px;
 `
+const Input = forwardRef((props, ref) => {
+  return <StyledInput {...props} ref={ref} />
+})
 
-export default function Input(props) {
-  return <StyledInput {...props} />
-}
+Input.displayName = 'Input'
+
+export default Input
